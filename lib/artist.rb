2d0f89c0492c54d2d47dcb5710c57c563bd713e0
Ross Enriques <rtyenriques@gmail.com>
2020-06-02 04:20:33 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,12 @@
+class Song
+attr_accessor :artist
+
+
+
+
+end
+
+class Artist
+attr_accessor :name
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,7 @@
+class Post
+  attr_accessor :title
+end
+
+class Author
+  attr_accessor :name
+end
